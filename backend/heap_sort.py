1031a9a69059ff144f92e53ff80edf63de9b103d
Rocita Rana <rocitarana@Rocitas-MacBook-Pro.local>
2025-04-21 17:48:44 -0400

    heapify algo for lists. need to read from json now and store key value pairs in array

--- backend/heap_sort.py
+++ backend/heap_sort.py
@@ -1,8 +1,31 @@
+import json
+
 def sort_by_date(data):
     x = 2
 
-def sort_by_int(data):
-    x = 2
+def heapify(arr, n, i):
+    largest = i 
+    l = 2 * i + 1 
+    r = 2 * i + 2  
+
+    # If left child is larger than root
+    if l < n and arr[l] > arr[largest]:
+        largest = l
+
+    if r < n and arr[r] > arr[largest]:
+        largest = r
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heapSort(arr):
+    n = len(arr) 
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0] 
+        heapify(arr, i, 0)
 
 def sort_by_name(data):
     x = 2
