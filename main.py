419244da69d4fcfcc897a0eed766a9f27c9559c7
Noah Rosenberger <noahrosenberger@ufl.edu>
2025-04-21 12:36:22 -0400

    removed comments

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ if __name__ == '__main__':
     ok = read_fruit_prices()
     words = []
     prices = []
-    for tup in ok:
-        words.append(tup[0])
-        prices.append(tup[1])
 
 
     try:
@@ -28,21 +25,12 @@ if __name__ == '__main__':
     #     print(e)
 
     # try:
-    #     result = countingSort([4, 9, 20, 3, 7, 4, 8])  # Valid
-    #     print(result)
-    #     # Large random sequence
     #
     #
     #     result = countingSort([42, 17, 99, 1, 78, 56, 32, 8, 21, 5, 89, 63, 14, 92, 31, 77, 46])
     #     print(result)
     #
-    #     # Reverse-sorted list
-    #     result = countingSort([100, 90, 80, 70, 60, 50, 40, 30, 20, 10])
-    #     print(result)
     #
-    #     # Already sorted list
-    #     result = countingSort([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-    #     print(result)
     #
     #     # List with duplicates
     #     result = countingSort([5, 3, 9, 5, 3, 9, 2, 8, 8, 1, 4, 4])
@@ -56,8 +44,7 @@ if __name__ == '__main__':
     #     result = countingSort(list(range(1000, 0, -1)))  # Sorting from 1000 down to 1
     #     print(result)
     #
-    #
-    #     #sort(["a", "b", 3])  # Raises TypeError
+
     # except Exception as e:
     #     print(e)
     #
